ed957c591c1808712b1ec999c90b5c97eb7e75de
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-iMac.local>
2023-04-17 13:51:10 +0200

    feat: api error handling

--- api/app.js
+++ api/app.js
@@ -1,8 +1,30 @@
 require("dotenv").config();
 
 const express = require("express");
+const logger = require("morgan");
+const createError = require("http-errors");
 
 const app = express();
+
+app.use(logger("dev"));
+
+//** Error handling */
+
+app.use((req, res, next) => next(createError(404, "Route not found")));
+
+app.use((error, req, res, next) => {
+  console.error(error);
+
+  if (!error.status) {
+    error = createError(500, error);
+  }
+  const data = {
+    message: error.message
+  };
+
+  res.status(error.status).json(data);
+
+});
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => console.info(`Application is running at port ${port}`));
