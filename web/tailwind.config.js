01ba98fbb6a25848289d6cbb1ea2c1f124faa350
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-iMac.home>
2023-04-26 12:31:17 +0200

    feat: tailwind config

---
+++ web/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "orange": "#FF914D",
+        "green": "#809D7B"
+      }
+    },
+  },
+  plugins: [],
+}
