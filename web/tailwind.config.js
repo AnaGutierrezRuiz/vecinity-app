56ac9adb27c0b11c5319a696265b0e3a58c2ae8e
Ana Gutiérrez Ruiz <agutrui@hotmail.com>
2023-05-12 12:17:43 +0200

    config: added tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,15 +1,71 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         "orange": "#FF914D",
-        "green": "#809D7B"
-      }
+        "green": "#809D7B",
+        "greensoft": "#E0E8DF",
+        "orangesoft": "#F7A877",
+        "graysoft": "#F3F4F6"
+      },
+      animation: {
+        text: "text 5s ease infinite",
+        "gradient-x": "gradient-x 15s ease infinite",
+        "gradient-y": "gradient-y 15s ease infinite",
+        "gradient-xy": "gradient-xy 15s ease infinite",
+      },
+      fontFamily: {
+        sans: ["Roboto", "Helvetica", "Arial", "sans-serif"]
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+        "gradient-y": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "center top"
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "center center"
+          }
+        },
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center"
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center"
+          }
+        },
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center"
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center"
+          }
+        }
     },
   },
+},
   plugins: [],
 }
+
