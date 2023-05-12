63419196e283cba525d61001ac4116775a1df648
Ana Gutiérrez Ruiz <agutrui@hotmail.com>
2023-05-12 12:52:55 +0200

    feat: layout and header components

---
+++ web/src/components/layout/PageLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../header/Header';
+
+function PageLayout({ title, children }) {
+  return (
+    <>
+      <div className='py-0.5 pb-28 top-0 z-49 w-full flex flex-col content-center dark:bg-gray-800'>
+        <Header title={title} />
+        <div className="dark:bg-gray-800 h-screen">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PageLayout;
